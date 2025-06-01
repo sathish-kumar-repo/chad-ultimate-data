d99436dee7779ac45f2c2dcfa19f700a4ac470d0
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-06-01 21:14:00 +0530

    updated

--- Image/.js
+++ Image/.js
@@ -1,3 +1,6 @@
-for (let i = 12; i <= 36; i++) {
-  console.log(`"images (${i}).jpg",`);
+// for (let i = 37; i <= 87; i++) {
+//   console.log(`"images (${i}).jpg",`);
+// }
+for (let i = 1; i <= 12; i++) {
+  console.log(`"download (${i}).jpg",`);
 }
